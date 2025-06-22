8adddfe1c98c1ae6bff59258d16f6304ef0ee681
Aakash Gupta <uchanahome8@gmail.com>
2025-06-22 19:50:30 +0000

    Completed 5.2: If the file is opened in APPEND_MODE, irrespective of lseek, when a write occurs, the pointer is moved to end of file.

---
+++ chap5/5.2.c
@@ -0,0 +1,46 @@
+/* Write a program that opens an existing file for writing with the O_APPEND flag, and
+ * then seeks to the beginning of the file before writing some data. Where does the
+ * data appear in the file? Why?
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int exercise_5_2(char* filename) {
+    const int fd = open(filename, O_RDWR | O_APPEND, 0644);
+    if (fd < 0) {
+        perror("open");
+        close(fd);
+        return 1;
+    }
+
+    if (lseek(fd, 0, SEEK_SET) < 0) {
+        perror("lseek");
+        close(fd);
+        return 1;
+    }
+
+    if (write(fd, "123", 3) < 0) {
+        perror("write");
+        close(fd);
+        return 1;
+    }
+
+    char BUFF[1024];
+    ssize_t len;
+    while ((len = read(fd, BUFF, sizeof(BUFF))) != 0) {
+        if (len < 0) {
+            perror("read");
+            close(fd);
+            return 1;
+        }
+
+        write(1, BUFF, len);
+    }
+
+    close(fd);
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-int exercise_4_2(int argc, char** argv);
+int exercise_5_2(char* filename);
 
 int main() {
     char* args[] = {"cpp", "/IdeaProjects/linux_programming_interface/cache/source.txt", "/IdeaProjects/linux_programming_interface/cache/destination.txt" };
-    exercise_4_2(3, args);
+    exercise_5_2(args[1]);
     return 0;
 }
