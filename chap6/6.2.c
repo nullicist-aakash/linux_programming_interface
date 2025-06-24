2db19f63a80ef9b0ec582da251bf65ba02170168
Aakash Gupta <uchanahome8@gmail.com>
2025-06-24 18:24:38 +0000

    Completed 6.2: Got SIGBUS

---
+++ chap6/6.2.c
@@ -0,0 +1,36 @@
+/*
+ * Write a program to see what happens if we try to longjmp() into a function that has
+ * already returned
+ */
+
+#include <setjmp.h>
+#include <stdio.h>
+
+static jmp_buf env;
+
+void foo() {
+    switch (setjmp(env)) {
+        case 0:
+            printf("Normal execution\n");
+            return;
+        default:
+            printf("Came back from somewhere\n");
+            break;
+    }
+
+    printf("Came back\n");
+}
+
+void goo() {
+    int x = 1;
+    printf("Inside goo %d\n", x);
+}
+
+int exercise_6_2() {
+    foo();
+    goo();
+    longjmp(env, 3);
+}
+
+
+// Got error of SIGBUS.
--- main.c
+++ main.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int exercise_5_3(char* filename, int num_bytes, int append_flag);
+int exercise_6_2();
 
 int main(int argc, char** argv) {
-    if (argc > 2)
-        exercise_5_3(argv[1], (int)strtol(argv[2], &argv[3], 10), argc == 3);
-    else
-        printf("Usage: %s filename num-bytes [x]\n", argv[0]);
-
+    exercise_6_2();
     return 0;
 }
