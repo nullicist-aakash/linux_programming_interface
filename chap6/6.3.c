4f5a5e58201a681d066e8f09808a20bdecc6321b
Aakash Gupta <uchanahome8@gmail.com>
2025-06-24 18:58:14 +0000

    Completed 6.3: seta dn unsetenv with putenv and getenv

---
+++ chap6/6.3.c
@@ -0,0 +1,47 @@
+/*
+ * Implement setenv() and unsetenv() using getenv(), putenv(), and, where necessary,
+ * code that directly modifies environ. Your version of unsetenv() should check to see
+ * whether there are multiple definitions of an environment variable, and remove
+ * them all (which is what the glibc version of unsetenv() does).
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern char **environ;
+
+int exercise_6_3_setenv(const char *name, const char *value, int overwrite) {
+    if (getenv(name) != NULL && !overwrite) {
+        return 0;
+    }
+
+    char* env = malloc(strlen(name) + strlen(value) + 2);
+    if (env == NULL) {
+        perror("malloc");
+        return 1;
+    }
+    memcpy(env, name, strlen(name));
+    env[strlen(name)] = '=';
+    memcpy(env + strlen(name) + 1, value, strlen(value) + 1);
+    env[strlen(name) + strlen(value) + 1] = '\0';
+    return putenv(env);
+}
+
+int exercise_6_4_unsetenv(const char *name) {
+    const size_t name_len = strlen(name);
+
+    int count = 0;
+    for (char** p = environ; *p != NULL; p++)
+        ++count;
+
+    for (int i = 0; i < count; i++) {
+        if (strncmp(name, environ[i], name_len) != 0 || environ[i][name_len] != '=')
+            continue;
+
+        environ[i] = environ[--count];
+        environ[i] = NULL;
+    }
+
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int exercise_6_2();
+int exercise_6_3_setenv(const char *name, const char *value, int overwrite);
+int exercise_6_4_unsetenv(const char *name);
 
 int main(int argc, char** argv) {
-    exercise_6_2();
+    printf("%d\n", getenv("abc") == NULL);
+    exercise_6_3_setenv("abc", "def", 0);
+    printf("%s\n", getenv("abc"));
+    exercise_6_3_setenv("abc", "ghi", 1);
+    printf("%s\n", getenv("abc"));
+    exercise_6_4_unsetenv("abc");
+    printf("%d\n", getenv("abc") == NULL);
     return 0;
 }
